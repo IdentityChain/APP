b05e79a0584bb80ef553b036d249134409f91545
李晓明 <lixiaoming@lixiaomingdeMBP.lan>
2018-09-25 10:21:29 +0800

    重新更新buildAPP

--- vux/src/config/config.js
+++ vux/src/config/config.js
@@ -5,10 +5,6 @@ module.exports = {
   // apiServer: 'http://172.20.10.6:9088',
   // apiServer: 'http://192.168.43.104:9088',
   // deployAPP: false,
-<<<<<<< HEAD
-  // apiServer: 'http://localhost:9088',
-=======
-  deployAPP: true,
->>>>>>> 66c829f7b66adc0f9fb378c4e19d4b4f01bc7550
+  // deployAPP: true,
   useAuth: true
 }
